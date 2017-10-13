6d393323936c8d8ba3b5ef02a33e661a9873b1d6
btopro <btopro@outlook.com>
2017-10-13 10:13:08 -0400

    updated from what was in drupal

--- lrnapp-book.php
+++ lrnapp-book.php
@@ -4,54 +4,191 @@
  * Code for LRNApp book
  */
 
+// hook up the book service
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__.'/services/LRNAppBookService.php');
+require_once(dirname(__FILE__).'/LRNAppBookProperties.php');
+
 /**
- * Callback for apps/lrnapp-book/data.
- * @param  string $machine_name machine name of this app
- * @return array               data to be json encoded for the front end
+ *  * Callback for apps/lrnapp-book/api/page/%.
  */
-function _lrnapp_book_data($machine_name, $app_route, $params, $args) {
-  $return = array();
-  // check for node being set and being a number
-  if (isset($_GET['node']) && is_numeric($_GET['node'])) {
-    // load and see if its
-    $node = node_load($_GET['node']);
-    // validate they have access to see this otherwise bail
-    if (!node_access('view', $node)) {
-      $status = 404;
+function _lrnapp_book_findone($machine_name, $app_route, $params, $args) {
+  $return = array('status' => 200);
+  $method = $_SERVER['REQUEST_METHOD'];
+  $service = new LRNAppBookService();
+  // Find out if there is a nid specified
+  if ($method == 'GET' || $method == 'PUT' || $method == 'DELETE') {
+    if (isset($args[2])) {
+      $nid = $args[2];
+      if ($nid) {
+        // if it's a GET method then we can return the node.
+        switch ($method) {
+          case 'GET':
+            $return['data'] = $service->getPage($args[2]);
+            // update the last viewed history timestamp
+            if ($GLOBALS['user']->uid && $return['data']->id) {
+              db_merge('history')
+                ->key(array(
+                  'uid' => $GLOBALS['user']->uid,
+                  'nid' => $return['data']->id,
+                ))
+                ->fields(array('timestamp' => REQUEST_TIME))
+                ->execute();
+             }
+            break;
+          case 'PUT':
+            $post_data = file_get_contents("php://input");
+            $post_data = json_decode($post_data);
+            // try to update the node
+            try {
+              $update = $service->updatePage($post_data, $args[2]);
+              $return['data'] = $update;
+            }
+            // if it fails we'll add errors and return 500
+            catch (Exception $e) {
+              $return['status'] = 500;
+              $return['errors'][] = $e->getMessage();
+            }
+            break;
+          case 'DELETE':
+            try {
+              $delete = $service->deletePage($args[2]);
+            }
+            catch (Exception $e) {
+              $return['status'] = 500;
+              $return['errors'][] = $e->getMessage();
+            }
+            break;
+          default:
+            # code...
+            break;
+        }
+      }
+      else {
+        $return['status'] = 404;
+      }
+    }
+  }
+
+  return $return;
+}
+
+/**
+ * Callback for apps/lrnapp-book/api/page/create-stub
+ */
+function _lrnapp_book_create_stub($machine_name, $app_route, $params, $args) {
+  $return = array('status' => 200);
+  $method = $_SERVER['REQUEST_METHOD'];
+
+  // Find out if there is a nid specified
+  if ($method == 'POST') {
+    $post_data = file_get_contents("php://input");
+    if ($post_data) {
+      $post_data = json_decode($post_data);
+      $service = new LRNAppBookService();
+      try {
+        $page = $service->createStubPage($post_data);
+        $return['status'] = 201;
+        $return['data'] = $page;
+      }
+      // if it fails we'll add errors and return 500
+      catch (Exception $e) {
+        $return['errors'][] = $e->getMessage();
+      }
+    }
+    else {
+      $return['status'] = 422;
+      $return['errors'][] = t('Parent or book id missing');
     }
   }
   else {
-    $node = _mooc_helper_active_outline();
+    $return['status'] = 400;
+    $return['errors'][] = t('Bad request. Method not allowed.');
   }
+
+  return $return;
+}
+
+/**
+ * Callback for apps/lrnapp-book/api/outline.
+ * @param  string $machine_name machine name of this app
+ * @return array               data to be json encoded for the front end
+ */
+function _lrnapp_book_outline_data($machine_name, $app_route, $params, $args) {
+  $return = array();
+  $items = array();
+  $status = 404;
+  $service = new LRNAppBookService();
+  $node = $service->loadActiveNode();
+  if (isset($node->nid)) {
+    $page = $service->getPage($node->nid);
+    $outline = $service->getOutline($node->nid);
+    $items = $service->encodeOutline($outline, $app_route . '/');
+    $return['outlineTitle'] = $page->attributes->title;
+  }
+  $return['items'] = $items;
   // verify this is a node
   if (isset($node->nid)) {
-    // render the node to content
-    $GLOBALS['moocappbook'] = $node;
-    // load the navigation block; obviously not this way
-    $block = block_load('mooc_nav_block', 'mooc_nav');
-    $nav =  _block_get_renderable_array(_block_render_blocks(array($block)));
-    $nav['mooc_nav_block_mooc_nav']['#markup'] = str_replace('href="' . base_path() .'node/', 'on-click="changePage" data-node="', $nav['mooc_nav_block_mooc_nav']['#markup']);
-    // load the breadcrumb; obviously not this way
-    $block = block_load('mooc_helper_book_nav', 'book_sibling_nav');
-    $pathing =  _block_get_renderable_array(_block_render_blocks(array($block)));
-    $return['content']['breadcrumb'] = drupal_render($pathing);
-    $return['content']['nav'] = drupal_render($nav);
-    $return['content']['body'] = check_markup('<h2>' . $node->title . '</h2>' . $node->body['und'][0]['value'], $node->body['und'][0]['format']);
-    // load associations from cache
-    if (isset($node->book['mlid'])) {
-      $associations = _book_cache_get_associations($node->book);
-      // previous link
-      if (isset($associations['prev'])) {
-        $return['prev'] = str_replace('node/', '', $associations['prev']['link_path']);
-      }
-      // next link
-      if (isset($associations['next'])) {
-        $return['next'] = str_replace('node/', '', $associations['next']['link_path']);
-      }
-    }
     $status = 200;
   }
-  //
+
+  return array(
+    'status' => $status,
+    'data' => $return
+  );
+}
+
+/**
+ * Callback for apps/lrnapp-book/api/book.
+ * @param  string $machine_name machine name of this app
+ * @return array               data to be json encoded for the front end
+ */
+function _lrnapp_book_book_data($machine_name, $app_route, $params, $args) {
+  $return = array();
+  $items = array();
+  $status = 404;
+  $service = new LRNAppBookService();
+  $node = $service->loadActiveNode();
+  if (isset($node->nid)) {
+    $page = $service->getPage($node->nid);
+    // treat as a book outline instead of in depth outline
+    $outline = $service->getOutline($node->nid, TRUE);
+    $items = $service->encodeOutline($outline, $app_route . '/');
+  }
+  $return = array(
+    "items" => $items
+  );
+  // verify this is a node
+  if (isset($node->nid)) {
+    $status = 200;
+  }
+
+  return array(
+    'status' => $status,
+    'data' => $return
+  );
+}
+
+/**
+ * Callback for apps/lrnapp-book/api/book.
+ * @param  string $machine_name machine name of this app
+ * @return array               data to be json encoded for the front end
+ */
+function _lrnapp_book_page_data($machine_name, $app_route, $params, $args) {
+  $status = 404;
+  $service = new LRNAppBookService();
+  $node = $service->loadActiveNode();
+  if (isset($node->nid)) {
+    $page = $service->getPage($node->nid);
+  }
+  $return = array(
+    'title' => _lrnapp_book_render_active_title(),
+    'content' => _lrnapp_book_render_active_node(),
+    'page' => $page,
+  );
+  if (!empty($return['title'])) {
+    $status = 200;
+  }
   return array(
     'status' => $status,
     'data' => $return
